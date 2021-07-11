7501303b3d59673b30a633c717f75749d95e7847
Justin Perez <perezjrp936@gmail.com>
2021-07-11 18:01:18 -0400

    got generate password function to work and display passwords with different characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,34 @@ var upperCase = false;
 var lowerCase = false;
 var numsBool = false;
 var specialsBool = false;
+var randomNum= 0;
 
 var generatePassword = function(){
   var pswd = "";
-  var setPswdLength = window.prompt("How long would you like your password to be? Enter a number greater than 8");
+  var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890~`!@#$%^&*()_-+={[}],.<>?";
+  var setPswdLength = window.prompt("How long would you like your password to be? Enter a number of 8 or greater");
   setPswdLength = parseInt(setPswdLength);
   if(setPswdLength >= 8){
+    randomNum = 0;
     passwordCriteria();
-    passwordCreate(pswd);
+    for(var i = 0; i<setPswdLength; i++){
+      if(randomNum === 1){
+        pswd += charset.charAt(Math.floor(Math.random() * 25));
+      }
+      else if(randomNum === 2){
+        pswd += charset.charAt(Math.floor(Math.random() * 51));
+      }
+      else if(randomNum === 3){
+        pswd += charset.charAt(Math.floor(Math.random() * 61));
+      }
+      else if(randomNum === 4){
+        pswd += charset.charAt(Math.floor(Math.random() * charset.length));
+      }
+      else{
+        break;
+      }
+    }
+    console.log(pswd);
     return pswd;
   }
   else{
@@ -19,36 +39,29 @@ var generatePassword = function(){
   }
 }
 var passwordCriteria = function(){
-  var confirmUpper = window.prompt("Would you like uppercase letters in your password? enter y for yes and leave blank for no");
-  if(confirmUpper === "y"){
-    upperCase = true;
-  }
   var confirmLower = window.prompt("Would you like lowercase letters in your password? enter y for yes and leave blank for no");
   if(confirmLower === "y"){
     lowerCase = true;
+    randomNum ++;
+  }
+  var confirmUpper = window.prompt("Would you like uppercase letters in your password? enter y for yes and leave blank for no");
+  if(confirmUpper === "y"){
+    upperCase = true;
+    randomNum ++;
   }
   var confirmNums = window.prompt("Would you like numbers in your password? enter y for yes and leave blank for no");
   if(confirmNums === "y"){
     numsBool = true;
+    randomNum ++;
   }
   var confirmSpecials = window.prompt("Would you like numbers in your password? enter y for yes and leave blank for no");
   if(confirmSpecials === "y"){
     specialsBool = true;
+    randomNum ++;
   }
+  console.log(randomNum);
 }
-var passwordCreate = function(input){
-  var uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var lowers = "abcdefghijklmnopqrstuvwxyz";
-  var nums = "1234567890";
-  var specials = "~`!@#$%^&*()_-+={[}],.<>?";
-  var passwordString="";
-  for(var i = 0; i<input.length; i++){
-    
-  }
-}
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
